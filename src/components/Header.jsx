895a8c5b097df60fdb8eeffcb4441e474f0ea480
chandrakantdubey <919chandrakant@gmail.com>
2023-07-08 21:53:32 +0530

    corrected the dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,30 +3,31 @@ import login from '../assets/user-icon.png'
 
 export default function Header() {
   return (
-    <header className="container navbar navbar-expand-md app__wrapper align-items-center py-3">
-      <Link to="/" className="col header__brand">
-        #VansLife
-      </Link>
-      <nav className="col navbar navbar-expand-sm">
+    <header className="container-fluid navbar navbar-expand-md app__wrapper align-items-center py-3">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          #VansLife
+        </Link>
         <button
-          className="navbar-toggler ms-auto"
+          className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+          data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className="navbar-nav d-flex gap-2 ms-auto">
-            <NavLink to="host" className={({isActive}) => isActive ? `active nav-item nav-link`: `nav-item nav-link`}>Host</NavLink>
-            <NavLink to="about" className={({isActive}) => isActive ? `active nav-item nav-link`: `nav-item nav-link`}>About</NavLink>
-            <NavLink to="vans" className={({isActive}) => isActive ? `active nav-item nav-link`: `nav-item nav-link`}>Vans</NavLink>
-            <NavLink to="login" className={({isActive}) => isActive ? `active nav-item nav-link`: `nav-item nav-link`}>
+          <nav className="navbar-nav ms-auto">
+            <NavLink to="host" className={({isActive}) => isActive ? `active nav-item nav-link m-auto`: `nav-item nav-link m-auto`}>Host</NavLink>
+            <NavLink to="about" className={({isActive}) => isActive ? `active nav-item nav-link m-auto`: `nav-item nav-link m-auto`}>About</NavLink>
+            <NavLink to="vans" className={({isActive}) => isActive ? `active nav-item nav-link m-auto`: `nav-item nav-link m-auto`}>Vans</NavLink>
+            <NavLink to="login" className={({isActive}) => isActive ? `active nav-item nav-link m-auto`: `nav-item nav-link m-auto`}>
               <img src={login} alt="user icon" />
             </NavLink>
-          </div>
+          </nav>
         </div>
-      </nav>
+      </div>
     </header>
   );
 }
